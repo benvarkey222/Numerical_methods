ad06ea017c651b0f1fe8339e4a299c22d53ee421
benvarkey222 <benvarkey222@gmail.com>
2025-02-15 14:46:26 -0500

    added Gauss

--- computing4.py
+++ computing4.py
@@ -3,9 +3,14 @@
 
 #Problem 1
 #changes matrix to be upper tri and returns the lower tri
-def Triangulate(matrix, b):
+def Triangulate(matrix):
     #check if n by n and if b dimesions line up
-    lower = [[1,0,0],[0,1,0],[0,0,1]]
+    lower = []
+    for i in range(len(matrix)):
+        row = [0]*len(matrix)
+        row[i] = 1
+        lower.append(row)
+    
     for col in range(len(matrix[0])):
         for row in range(col+1, len(matrix)):
             mult = matrix[row][col]/matrix[col][col]
@@ -46,13 +51,26 @@ def LUSolve(A, b):
     print(y)
     return BackwardSub(A,y)
 
+def Gauss(matrix, b):
+    lower = [[1,0,0],[0,1,0],[0,0,1]]
+    for col in range(len(matrix[0])):
+        for row in range(col+1, len(matrix)):
+            mult = matrix[row][col]/matrix[col][col]
+            for rIndex in range(col, len(matrix[0])):
+                matrix[row][rIndex] = matrix[row][rIndex] - mult*matrix[col][rIndex]
+            b[row] = b[row] - mult*b[col]
+            lower[row][col] = mult
+    return BackwardSub(matrix,b)
 
 
 
 
-matrix = [[10,2,-1],[-3,-5,2],[1,1,6]]
-b = [27,-61.5,-21.5]
-sol = LUSolve(matrix, b)
-print(sol)
+
+matrix = [[1,0,1/3,0],[0,1,3,-1],[3,-3,0,6],[0,2,4,-6]]
+# b = [27,-61.5,-21.5]
+lower = Triangulate(matrix)
+print(matrix)
+print("\n\n")
+print(lower)
 
 
