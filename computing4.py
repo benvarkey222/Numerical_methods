8d861f68a64d60d76d01a9dce86d257c9f230838
benvarkey222 <benvarkey222@gmail.com>
2025-02-19 00:20:01 -0500

    made matrix_mult function

--- computing4.py
+++ computing4.py
@@ -1,5 +1,5 @@
 
-
+import numpy as np
 
 #Problem 1
 #changes matrix to be upper tri and returns the lower tri
@@ -63,14 +63,107 @@ def Gauss(matrix, b):
     return BackwardSub(matrix,b)
 
 
+def Doolittle(A, uD, lD):
+    #intialize U and L as emtry matices 
+    #enters diagionals for each one
+    #for 0 to uD or lD length (k)
+        #check whether entry is defined in uD or aD
+        #if defined in uD
+            #for k+1 to uD length (i)
+                #define (a) entry in k column
+                #for 1 to k-1 (m)
+                    #subtract (entries in l i row * entries in u k col) from 
+                    # entries in k column of a 
+                #divind by uD entry
+                #add entry to l ik entry
+        #else if defined in lD
+            #for k+1 to uD length (j)
+                #define (a) entry in k row
+                #for 1 to k-1 (m)
+                    #subtract (entries in l k row * entries in u j col) from 
+                    # entries in k column of a 
+                #divind by lD entry
+                #add entry to U kj entry
+    n = len(uD)
+    U = np.zeros((n,n))
+    L = np.zeros((n,n))
+
+    
+    #intialize U and L as emtry matices 
+    #enters diagionals for each one
+    for i in range(n):
+        U[i,i] = uD[i]
+        L[i,i] = lD[i]
+    
+    for k in range(n):
+        if U[k,k]!=0: #defined in uD
+            for i in range(k, n):
+                l_entry = A[i,k]
+                for m in range(0, k-2): #check indices
+                    l_entry= l_entry - (L[i,m]*U[m,k])
+                l_entry = l_entry/U[k,k]
+                L[i,k] = l_entry
+            for j in range(k+1, n):
+                u_entry = A[k,j]
+                for m in range(0, k-2): #check indices
+                    if L[k][m] == 0:
+                        print("l none")
+                        print(m)
+                        print(k)
+                    elif U[m][j] ==0:
+                        print("u none")
+                    u_entry= u_entry - (L[k,m]*U[m,j])
+                u_entry = u_entry/L[k,k]
+                U[k,j] = u_entry
+                
+        elif L[k,k] != 0 : #defined in lD
+            for j in range(k, n):
+                u_entry = A[k,j]
+                for m in range(0, k-2): #check indices
+                    if L[k][m] == 0:
+                        print("l none")
+                        print(m)
+                        print(k)
+                    elif U[m][j] ==0:
+                        print("u none")
+                    u_entry= u_entry - (L[k,m]*U[m,j])
+                u_entry = u_entry/L[k,k]
+                U[k,j] = u_entry
+            for i in range(k+1, n):
+                l_entry = A[i,k]
+                for m in range(0, k-2): #check indices
+                    l_entry= l_entry - (L[i,m]*U[m,k])
+                l_entry = l_entry/U[k,k]
+                L[i,k] = l_entry    
+    return L,U
+
+def matrix_mult(A, B):
+    product = 0
+    if len(A[0]) == len(B): #if As column number = Bs row number
+        product  = np.zeros((len(A), len(B[0])))
+        (rnum,cnum) = product.shape
+        for r in range(rnum):
+            for c in range(cnum):
+                row_A = A[r, :]  # Row 2 of matrix A (indexing starts at 0)
+                col_B = B[:, c]
+                product[r,c] = sum(row_A*col_B)
+    return product
 
 
+matrix = np.array([[5,7,6,5],[7,10,8,7],[6,8,10,9],[5,7,9,10]])
+lD = [1,1,1,1]
+uD = [0, 0, 0, 0]
+L,U = Doolittle(matrix,uD,lD)
+print("L:", L)
+print("U: ", U)
+print(matrix_mult(L,U))
 
-matrix = [[1,0,1/3,0],[0,1,3,-1],[3,-3,0,6],[0,2,4,-6]]
-# b = [27,-61.5,-21.5]
-lower = Triangulate(matrix)
-print(matrix)
-print("\n\n")
-print(lower)
+
+# matrix = [[1,0,1/3,0],[0,1,3,-1],[3,-3,0,6],[0,2,4,-6]]
+# # b = [27,-61.5,-21.5]
+# lower = Triangulate(matrix)
+# print(matrix)
+# print("\n\n")
+# print(lower)
 
 
